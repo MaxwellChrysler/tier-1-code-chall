5eed0a8af66028d8e8399228abd9dca9c9b0a00d
Maxwell Chrysler <maxwellchrysler@hotmail.com>
2023-05-03 14:07:01 -0500

    fixed return within the sumTiles function. Made it so only the first value was to be returned

--- client.js
+++ client.js
@@ -6,28 +6,25 @@ You’ve been given an index.html file and this starter js file.
 */
 
 console.log('Step 0 complete! client.js loaded!');
-            
+
 // Write your code here!
 /*  */
 // 1. Start with the number 29 and set that equal to a variable named ‘highScore’;
-let highsScore = 29;
+let highScore = 29; // I spelt this as highsScore so with two S's in there
 
 
 // 2. Make a new empty array called myScrabbleTiles.
+
 let myScrabbleTiles = [];
-
-console.log(myScrabbleTiles);
-
 
 // A function to push the values on to the objects in the array
 // I moved this function up here in an attempt to debug
-function addTiles(letter,score){ 
+function addTiles(letter, score) {
   // 
   let addATile = {
     letter: letter,
     score: score
   }
-
   myScrabbleTiles.push(addATile);
 }
 
@@ -70,35 +67,44 @@ addTiles('F', 4);
 // 6. Complete this function. It needs to be given an array of tile objects. The function will use a for-loop and return the sum of all the scores in the given array.
 function sumTiles(anArray) { // MAKE SURE TO RETURN
   let sum = 0;
-  for(i=0;i<anArray.length;i++){
+  for (i = 0; i < anArray.length; i++) {
     sum = sum + anArray[i].score; //
-  return sum;
+    //return sum; This return stopped the loop from going on and the code just stopped because the return ended it from looping
 
   }
-console.log(sum);
-// even following along in class im not sure why this isnt working
+  //console.log(sum);
+   return sum;
+  // even following along in class im not sure why this isnt working 
 }
-// its outputting not a number and I think its trying to access the letter values in my scrabble tile array
 
 console.log(sumTiles(myScrabbleTiles));
 
 
 // 7. Use the function above to get the total score for myScrabbleTiles and assign the value to a variable called "myScore".
+
 let myScore = sumTiles(myScrabbleTiles);
 
 // 8. Check whether or not your score is higher than the highScore.
-
-myScore= 30;
-
-if (myScore>highsScore){
-highsScore = myScore;
-}
-
-
-//else console.log(highscore);  
 // 8-1 If your score is higher, change highScore to the new high score.
 
+console.log('myScore:', myScore);
+console.log('highScore', highScore);
 
+if (myScore > highScore) {
+  highScore = myScore;
+}
 
-
+console.log('myScore:', myScore);
+console.log('highScore', highScore);
 // DONE!
+
+
+// Issues with the code :
+// The code would only output the sum of the first value of the array (so just the first number)
+// Console logging sumTiles(myScrabbleTiles) would output undefined
+/* ERRORS 
+line 72: I used return within the function and that prevented the other values in the array to keep adding up
+line 77: comment that I couldn't figure out why it wasn't working and it was because the placement of the return
+line 85: the function sumTiles was not getting any values to output because return sum was just dumping into an area that myScore couldn't access
+
+*/
